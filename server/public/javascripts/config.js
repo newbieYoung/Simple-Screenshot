3bfdf3fcdc1ae7fdc7e5e05e95d90d4554a8e80a
NewYoung <657851574@qq.com>
2020-09-14 16:32:19 +0800

    小修改

--- server/public/javascripts/config.js
+++ server/public/javascripts/config.js
@@ -1,6 +1,7 @@
 /* global module */
 module.exports = {
   cors: [
-    'https://127.0.0.1:5500', 'http://127.0.0.1:5500'
+    'https://127.0.0.1:5500', 'http://127.0.0.1:5500',
+    'https://newbieyoung.github.io', 'http://newbieyoung.github.io'
   ] //允许跨域域名列表
 }
