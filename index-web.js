52af4c9717b9baa883b55bbcdc0c73cbcfacf758
NewYoung <657851574@qq.com>
2020-09-19 13:32:17 +0800

    Revert "小修改"
    This reverts commit 5ef67f9ea0d19f2dc4a698ef7f19d811c0ef1a32.

--- index-web.js
+++ index-web.js
@@ -768,13 +768,7 @@
         }
       }
 
-      if (['width', 'height'].includes(name)) { // 尺寸
-        if (node._rules.autoRenderWidth || !this._hasWidthNodes.includes(node.node)) {
-          //
-        } else {
-          style += name + ":" + this.getNodeStyle(node, name) + ";";
-        }
-      } else if (name == "background-image") { //背景图片
+      if (name == "background-image") { //背景图片
         style += this.parseBackgroundImage(this.getNodeStyle(node, name));
         if (node.after) {
           after += this.parseBackgroundImage(node.after[name]);
@@ -791,6 +785,10 @@
           before += name + ":" + node.before[name] + ";";
         }
       }
+    }
+
+    if (node._rules.autoRenderWidth || !this._hasWidthNodes.includes(node.node)) {
+      style += "width: auto;";
     }
 
     return {
