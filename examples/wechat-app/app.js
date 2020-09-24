748280dc7d74d52ad611bc0e0ab8b037a10dcccf
NewYoung <657851574@qq.com>
2020-09-24 20:02:39 +0800

    微信小程序 demo 修改

--- examples/wechat-app/app.js
+++ examples/wechat-app/app.js
@@ -3,18 +3,8 @@
 const sys = wx.getSystemInfoSync();
 App({
   onLaunch: function () {
-    wx.cloud.init({
-      env: this.globalData.cloudEnv,
-      traceUser: true
-    })
-
-    this.globalData.cloudDB = wx.cloud.database({
-      env: this.globalData.cloudEnv
-    });
   },
   globalData: {
-    systemInfo: sys,
-    cloudEnv: 'simplescreenshot-0tqah',
-    cloudDB: null
+    systemInfo: sys
   }
 })
